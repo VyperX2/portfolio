5767f6013c7013e8beff18a0ce0ffba1026b7cd6
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-20 08:49:30 +0300

    Set colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Blob from "@/components/Blob";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className="dark font-heading bg-background">
 				<Blob />
 				{children}
 			</body>
