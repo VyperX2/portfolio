b45ee504420eabf9521afabe7b5e58b05e7a8d9d
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-22 13:27:45 +0300

    Add Shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<Blob /> 
+					<Blob />
 					<Navbar />
 					{children}
 				</ThemeProvider>
