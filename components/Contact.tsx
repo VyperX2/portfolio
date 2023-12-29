6af7ac6618fb0d9470680a9ec66f3ff4e3207369
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-29 20:54:28 +0300

    Fix emailjs

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
 	const form = useRef(null);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 
 	const sendEmail = (e: any) => {
 		e.preventDefault();
 
 		emailjs
 			.sendForm(
-				`${process.env.SERVICE_ID}`,
+				`service_sdraqp7`,
 				"template_0e1pzik",
 				(form as any).current,
 				"Y2BiUWiXEE9GSnzDv"
@@ -23,17 +26,50 @@ export const Contact = () => {
 					console.log(error.text);
 				}
 			);
+		setMessage("");
+		setEmail("");
+		setName("");
 	};
 
 	return (
-		<form className=" mt-60" ref={form} onSubmit={sendEmail}>
-			<label>Name</label>
-			<input type="text" name="user_name" />
-			<label>Email</label>
-			<input type="email" name="user_email" />
-			<label>Message</label>
-			<textarea name="message" />
-			<input type="submit" value="Send" />
-		</form>
+		<section className="flex flex-col mt-60 gap-8 mb-5">
+			<h1
+				className="2xl:text-5xl lg:text-4xl md:text-4xl text-3xl text-primary font-semibold w-fit inline-block mr-4"
+				data-value="Contact Me"
+			>
+				Contact Me
+			</h1>
+			<form className="flex flex-col gap-6 " ref={form} onSubmit={sendEmail}>
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					name="user_name"
+					placeholder="Name"
+					className=" py-4 rounded-lg px-4 border border-primary  bg-primary/20 placeholder:text-white outline-none"
+				/>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					name="user_email"
+					placeholder="Email"
+					className=" py-4 rounded-lg px-4 border border-primary  bg-primary/20 placeholder:text-white outline-none"
+				/>
+				<textarea
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					name="message"
+					placeholder="Your Message"
+					className="py-4 rounded-lg px-4 border border-primary  bg-primary/20 h-80 placeholder:text-white outline-none"
+				/>
+
+				<input
+					className="w-fit border border-primary px-4 py-2 bg-transparent  rounded-lg hover:rotate-[-18deg] transition-all"
+					type="submit"
+					value="Send"
+				/>
+			</form>
+		</section>
 	);
 };
