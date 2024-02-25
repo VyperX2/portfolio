b285358499ef143afe3e3a72df93368bb36873da
VyperX2 <mateenahmed.qatar@gmail.com>
2024-02-25 13:28:29 +0300

    Add 3d effect to projectCard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,6 +3,7 @@ import { Anim4, Anim5 } from "@/animations/animation";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 interface CardProps {
 	i: number;
@@ -55,48 +56,61 @@ const ProjectCard = ({
 
 	return (
 		<div ref={cardContainer}>
-			<div
-				ref={container}
-				className={`card relative flex ${
-					i % 2 !== 0 ? "lg:flex-row" : "lg:flex-row-reverse"
-				} flex-col gap-8 p-4 border border-primary/80 bg-primary/10 transition-all hover:shadow-2xl hover:shadow-primary/20 rounded-2xl anim7`}
-			>
-				<a
-					href={`${site}`}
-					target="_blank"
-					className="relative lg:flex-1 h-96 rounded-2xl overflow-hidden projectimg anim6"
-				>
-					<Image
-						src={`/${src}`}
-						alt="projectImg"
-						layout="fill"
-						objectFit="cover"
-					/>
-					<div className="absolute h-full w-full bg-primary/20 hover:bg-transparent transition-all" />
-				</a>
-				<div className="flex-1 anim6 flex flex-col ">
-					<h2 className="2xl:text-3xl lg:text-2xl text-2xl text-primary font-semibold anim4">
-						{title}
-					</h2>
-					<p className="text-muted-foreground font-semibold sm:w-[85%]">{desc}</p>
-					<div className="flex items-center gap-4 lg:mt-auto mt-6">
-						<a
-							className=" border border-primary text-white px-4 py-2 bg-primary rounded-lg hover:rotate-[-18deg] transition-all"
-							href={site}
-							target="_blank"
+			<CardContainer className="w-full">
+				<CardBody className="w-full">
+					<div
+						ref={container}
+						className={`card relative flex ${
+							i % 2 !== 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+						} flex-col gap-8 p-4 border border-primary/80 bg-primary/10 transition-all hover:shadow-2xl hover:shadow-primary/20 rounded-2xl anim7`}
+					>
+						<CardItem
+							translateZ="70"
+							className="relative lg:flex-1 h-96 rounded-2xl overflow-hidden projectimg "
 						>
-							Visit Site
-						</a>
-						<a
-							className=" border border-primary px-4 py-2 bg-transparent  rounded-lg hover:rotate-[18deg] transition-all"
-							href={github}
-							target="_blank"
-						>
-							View Code
-						</a>
+							<a
+								href={`${site}`}
+								target="_blank"
+								className="relative lg:flex-1 h-96 rounded-2xl overflow-hidden projectimg anim6"
+							>
+								<CardItem className="w-full h-full" translateZ="100">
+									<Image
+										src={`/${src}`}
+										alt="projectImg"
+										layout="fill"
+										objectFit="cover"
+									/>
+								</CardItem>
+								<div className="absolute h-full w-full bg-primary/20 hover:bg-transparent transition-all" />
+							</a>
+						</CardItem>
+						<CardItem translateZ="50" className="flex-1 anim6 flex flex-col ">
+							<h2 className="2xl:text-3xl lg:text-2xl text-2xl text-primary font-semibold anim4">
+								{title}
+							</h2>
+							<p className="text-muted-foreground font-semibold sm:w-[85%]">
+								{desc}
+							</p>
+							<div className="flex items-center gap-4 lg:mt-auto mt-6">
+								<a
+									className=" border border-primary text-white px-4 py-2 bg-primary rounded-lg hover:rotate-[-18deg] transition-all"
+									href={site}
+									target="_blank"
+								>
+									Visit Site
+								</a>
+								<a
+									className=" border border-primary px-4 py-2 bg-transparent  rounded-lg hover:rotate-[18deg] transition-all"
+									href={github}
+									target="_blank"
+								>
+									View Code
+								</a>
+							</div>
+						</CardItem>
 					</div>
-				</div>
-			</div>
+				</CardBody>
+			</CardContainer>
 		</div>
 	);
 };
