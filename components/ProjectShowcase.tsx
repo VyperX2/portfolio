113d51e8778abe8b680751f24b118b7c0892f4e9
VyperX2 <mateenahmed.qatar@gmail.com>
2024-02-26 21:01:05 +0300

    Update card gap

--- components/ProjectShowcase.tsx
+++ components/ProjectShowcase.tsx
@@ -3,7 +3,7 @@ import ProjectCard from "./ProjectCard";
 
 const ProjectShowcase = () => {
 	return (
-		<div className="flex flex-col gap-20">
+		<div className="flex flex-col lg:gap-20 md:gap-40 gap-60   ">
 			{projectsData.map((project, i) => (
 				<ProjectCard {...project} i={i} />
 			))}
