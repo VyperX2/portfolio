906bfc090efa85ab4601e5f99700edff894d9b50
VyperX2 <mateenahmed.qatar@gmail.com>
2024-02-26 20:15:53 +0300

    Update project links

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -65,7 +65,7 @@ const projectsData = [
 		desc: "Cinemania, created using the TMDB API, is your gateway to the cinematic universe. Discover the latest releases, effortlessly search for your favorites, and delve into comprehensive actor profiles. Immerse yourself in the world of movies with this dynamic and information-rich platform",
 		src: "cinemania.png",
 		technologies: [],
-		site: "https://cinemania-eight.vercel.app/",
+		site: "https://cinemania-vyperx2.netlify.app",
 		github: "https://github.com/VyperX2/Cinemania",
 	},
 	{
@@ -73,7 +73,7 @@ const projectsData = [
 		desc: "A Youtube Clone, created using RapidApi that allows you to find the latest videos, discover categories, discover new channels, watch videos and browse comments.",
 		src: "yt-clone.png",
 		technologies: [],
-		site: "https://658dde84bae3c02b938ff5bc--genuine-marzipan-9acc5c.netlify.app/",
+		site: "https://mateen-tube.netlify.app/",
 		github: "https://github.com/VyperX2/yt-clone-react",
 	},
 	{
@@ -81,16 +81,9 @@ const projectsData = [
 		desc: "ShoeSphere is a simple shopping cart site created using React context that allows you to add or remove items, view items in your cart along with simple animations.  ",
 		src: "shoe.png",
 		technologies: [],
-		site: "https://e-commerce-website-gold-six.vercel.app/",
+		site: "https://shoe-sphere.netlify.app/",
 		github: "https://github.com/VyperX2/e-commerce-website",
 	},
-	// {
-	// 	title: "Cinemania",
-	// 	desc: "Find the latest movies,search for your favorite ones along with your favorite actors",
-	// 	src: "",
-	// 	technologies: [],
-	// 	site: "",
-	// },
 ];
 
 export { navItems, techArray, techTools, projectsData };
